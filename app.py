d7f1acbf94e56efaa9731a4bbe65dd1885a30f44
RWN-N <rowenbos@gmail.com>
2025-04-19 19:52:14 +0700

    apply new changes, such as new logging n middleware

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import os
 import logging
-from logging.config import dictConfig
-
+import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
+
 from langchain_ollama import ChatOllama
 
 from config.settings import Settings
@@ -13,56 +13,15 @@ from src.services.tts import TextToSpeech
 from src.services.nosql import MongoDBEngine
 
 from src.controllers.pawpal import pawpal_router
+from src.middleware import log_middleware
 
 
-# ENV Configuration
+# Load Up Global ENV Configuration
 CONFIG = Settings()
 
 # Logging
-os.makedirs("logs", exist_ok=True)
-dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "default": {
-                "format": (
-                    f"[{CONFIG.ENV_TYPE}:{CONFIG.APP.CONTAINER_NAME}] %(asctime)s [%(process)d] [%(levelname)s] %(name)s - %(module)s: %(message)s"
-                ),
-            }
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "default",
-                "level": "INFO",
-            },
-            "file": {
-                "class": "logging.FileHandler",
-                "filename": "logs/pawpal.log",
-                "formatter": "default",
-            },
-        },
-        "root": {
-            "level": "DEBUG",
-            "handlers": ["console", "file"],
-        },
-    }
-)
-disabled_loggers = (
-    "httpx",
-    "httpcore.http11",
-    "httpcore.connection",
-    "pdfminer",
-    "pymongo.topology",
-)
-for logger_name in logging.root.manager.loggerDict:
-    logger = logging.getLogger(logger_name)
-    if any(logger_name.startswith(disabled) for disabled in disabled_loggers):
-        logger.setLevel("ERROR")
-
-app_logger = logging.Logger(__name__)
-
+CONFIG.configure_logging()
+app_logger = logging.getLogger(__name__)
 
 # Initialize Services
 mongodb_engine = MongoDBEngine(
@@ -83,12 +42,7 @@ pawpal.set_agentic_cls(model=model, mongodb_engine=mongodb_engine)
 
 # App Router
 app = FastAPI()
-
-
-@app.get("/")
-async def home():
-    return {"message": "hello world, this is running"}
-
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
 
 app.include_router(
     pawpal_router(
@@ -98,3 +52,7 @@ app.include_router(
         logger=app_logger,
     )
 )
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=11080, log_config=None)
