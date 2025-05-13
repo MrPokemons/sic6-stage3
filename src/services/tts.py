4015aaea7e7c7c04df40265a16e86e9cde63a95c
RWN-N <rowenbos@gmail.com>
2025-05-13 14:00:48 +0700

    optimize tts get-client by calling the character_count once per each apikey

--- src/services/tts.py
+++ src/services/tts.py
@@ -3,7 +3,7 @@ import secrets
 import io
 import torch
 import soundfile as sf
-from typing import List, Optional
+from typing import List, Optional, Dict, Annotated
 from logging import Logger
 from transformers import AutoTokenizer, VitsModel
 from elevenlabs import AsyncElevenLabs, VoiceSettings
@@ -43,9 +43,13 @@ class FacebookMMSTextToSpeech(TextToSpeech):
 class ElevenlabsTextToSpeech(TextToSpeech):
     def __init__(self, api_keys: str):
         api_keys: List[str] = api_keys.split(";")
+
         self.clients: List[AsyncElevenLabs] = []
         for _api_key in api_keys:
             self.clients.append(AsyncElevenLabs(api_key=_api_key))
+
+        self.eligible_clients: Dict[AsyncElevenLabs, Annotated[int, "character count"]] = {}
+
         self.voice_id = "Xb7hH8MSUJpSbSDYk0k2"
         self.voice_settings = VoiceSettings(
             speed=0.95,
@@ -54,20 +58,27 @@ class ElevenlabsTextToSpeech(TextToSpeech):
             style=0.15,
         )
         self.model_id = "eleven_flash_v2_5"
-        # self.model_id = None
 
-    async def get_client(self) -> AsyncElevenLabs:
-        eligible_clients: List[AsyncElevenLabs] = []
+    async def set_eligible_clients(self) -> None:
+        if self.eligible_clients:  # already initiated
+            return
+
         for _client in self.clients:
             _subs_info = await _client.user.get_subscription()
-            if _subs_info.character_count <= 9500:
-                eligible_clients.append(_client)
-        if not eligible_clients:
+            self.eligible_clients[_client] = _subs_info.character_count
+
+    async def get_client(self, next_chars: int, *, max_chars: int = 10_000, offset: int = 100) -> AsyncElevenLabs:
+        await self.set_eligible_clients()
+        only_eligible_clients = [_client for _client, _count in self.eligible_clients.items() if _count <= (max_chars - next_chars - offset)]
+        if not only_eligible_clients:
             raise Exception("TTS: No more eligible clients from ElevenLabs")
-        return secrets.choice(eligible_clients)
+        chosen_client = secrets.choice(only_eligible_clients)
+        self.eligible_clients[chosen_client] += next_chars
+        return chosen_client
 
     async def synthesize_async(self, text: str) -> bytes:
-        client = await self.get_client()
+        text = text.strip()
+        client = await self.get_client(next_chars=len(text))
         audio_generator = client.text_to_speech.convert(
             voice_id=self.voice_id,
             text=text,
